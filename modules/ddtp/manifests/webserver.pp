b470911a7eae0e6e4e3117e40269dc4e08511a11
Andrey Skvortsov <andrej.skvortzov@gmail.com>
2017-11-17 02:10:35 +0300

    enable socache_shmcb apache2 module needed for SSL configuration

--- modules/ddtp/manifests/webserver.pp
+++ modules/ddtp/manifests/webserver.pp
@@ -52,6 +52,11 @@ class ddtp::webserver {
 		before => Service['apache2'],
 	}
 
+	file { '/etc/apache2/mods-enabled/socache_shmcb.load':
+		ensure => '../mods-available/socache_shmcb.load',
+		before => Service['apache2'],
+	}
+
 	file { '/etc/apache2/mods-enabled/ssl.load':
 		ensure => '../mods-available/ssl.load',
 		before => Service['apache2'],
