f56a721801c797f6a727a8e65b7536e53a5f5c08
Andrey Skvortsov <andrej.skvortzov@gmail.com>
2017-12-06 01:18:37 +0300

    make munin statistics available for everyone on the web

--- modules/ddtp/manifests/monitoring.pp
+++ modules/ddtp/manifests/monitoring.pp
@@ -24,7 +24,7 @@ class ddtp::monitoring::munin {
     package { 'munin': ensure => installed }
 
     # Override config file primarily to remove access restrictions
-    file { '/etc/apache2/conf-available/munin':
+    file { '/etc/apache2/conf-available/munin.conf':
         content => template("ddtp/apache2-munin.conf"),
         require => Package['munin','apache2'],
         notify => Service['apache2'],
